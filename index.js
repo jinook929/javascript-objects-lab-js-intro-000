6e572549e5776e1b7a73ab2098aad1752183c6ff
JINOOK JUNG <jinook929@gmail.com>
2020-11-06 01:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({}, object)
-  delete clone.key
+  delete clone[key]
   return clone
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object
 }
