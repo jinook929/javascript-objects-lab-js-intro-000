c0c3058bcb48b2b1159873aa9d597194c032d84e
JINOOK JUNG <jinook929@gmail.com>
2020-11-06 01:08:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipe = { egg : "3"}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var clone = object[key] = value
-  return object
+  var clone = Object.assign({}, object)
+  clone[key] = value
+  return clone
 }
